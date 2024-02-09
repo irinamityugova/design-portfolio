103db5417a9f642c437a1e14c4f05c6ed4b86564
Irina Mityugova <info@irinamityugova.me>
2024-02-08 21:40:57 -0700

    Unicorn interaction

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -8,4 +8,32 @@
   }, 500);
 })();
 
-console.log("functions.js end");
+// https://github.com/irinamityugova/design-portfolio/blob/master/assets/img/unicorn-1.png?raw=true
+
+var canvas = document.getElementById("bridge");
+var ctx = canvas.getContext("2d"),
+  img = new Image(),
+  radius = 70;
+
+img.onload = setup;
+img.src =
+  "https://github.com/irinamityugova/design-portfolio/blob/master/assets/img/unicorn-1.png?raw=true";
+
+function setup() {
+  // set image as pattern for strokeStyle
+  let pattern = ctx.createPattern(this, "no-repeat");
+  ctx.strokeStyle = pattern;
+  ctx.lineWidth = 120;
+
+  // for demo only, reveals image while mousing over canvas
+  canvas.onmousemove = function (e) {
+    var r = this.getBoundingClientRect(),
+      x = e.clientX - r.left,
+      y = e.clientY - r.top;
+
+    ctx.beginPath();
+    ctx.moveTo(x + Math.random() * 33 - 16, y + Math.random() * 33 - 16);
+    ctx.lineTo(x + Math.random() * 33 - 16, y + Math.random() * 33 - 16);
+    ctx.stroke();
+  };
+}
